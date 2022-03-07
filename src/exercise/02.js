bcedd51c1957cafe417e4aeb8f246af2e71bf0b9
Amanda Herschleb <amanda.herschleb@bonus.ly>
2022-03-07 14:50:37 -0700

    exercise 2 ec 4 optomize

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -5,21 +5,37 @@ import * as React from 'react'
 
 // custom hooks start with use
 // custom hook = a function that uses hooks
-function useLocalStorageState(key, defaultValue = '') {
+// third arg is an object of options 
+function useLocalStorageState(
+  key, 
+  defaultValue = '', 
+  { serialize = JSON.stringify, deserialize = JSON.parse } = {},
+) {
   // using a function here allows it to only be called once
   // on additional renderings the initial value is not called
   // only need to use a function if calling an expensive operation 
   // not needed if just using initialName prop
-  const [state, setState] = React.useState(
-    () => window.localStorage.getItem(key) || defaultValue
-  )
+  const [state, setState] = React.useState(() => {
+      const valueInLocalStorage = window.localStorage.getItem(key)
+    if (valueInLocalStorage) {
+      return deserialize(valueInLocalStorage)
+    }
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+  })
+
+  // if key changes we want to remove the old key from local storage
+  const prevKeyRef = React.useRef(key)
 
   React.useEffect(() => {
-    window.localStorage.setItem(key, state)
-  }, [key, state]) // only called when key or state change
+    const prevKey = prevKeyRef.current
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey)
+    }
+    prevKeyRef.current = key
+    window.localStorage.setItem(key, serialize(state))
+  }, [key, serialize, state]) // only called when key or state change
   
   return [state, setState]
-
 }
 
 function Greeting({initialName = ''}) {
