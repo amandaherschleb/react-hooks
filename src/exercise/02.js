ce52ae262a9f7c5be9a97b1a75598ca2589c6e2d
Amanda Herschleb <amanda.herschleb@bonus.ly>
2022-03-07 14:29:42 -0700

    exercise 2 ec 3

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,25 +3,32 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-
+// custom hooks start with use
+// custom hook = a function that uses hooks
+function useLocalStorageState(key, defaultValue = '') {
   // using a function here allows it to only be called once
   // on additional renderings the initial value is not called
   // only need to use a function if calling an expensive operation 
   // not needed if just using initialName prop
-  const [name, setName] = React.useState(
-    () => window.localStorage.getItem('name') || initialName
+  const [state, setState] = React.useState(
+    () => window.localStorage.getItem(key) || defaultValue
   )
 
-  // useEffect runs everytime a re-render happens if called without second arg
   React.useEffect(() => {
-    window.localStorage.setItem('name', name)
-  }, [name]) // now only called when name changes
+    window.localStorage.setItem(key, state)
+  }, [key, state]) // only called when key or state change
+  
+  return [state, setState]
+
+}
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageState()
 
   function handleChange(event) {
     setName(event.target.value)
   }
-  
+
   return (
     <div>
       <form>
